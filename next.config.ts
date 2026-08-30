64787b9723da26c09405347ce02f6441d0dc8c87
Wihan687 <izzatwihan24@gmail.com>
2026-08-31 00:42:14 +0700

    Revert "perdana"

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  allowedDevOrigins: [
-    "192.168.1.31",
-    "192.168.1.31:3000",
-    "localhost:3000",
-    "127.0.0.1:3000",
-  ],
+  /* config options here */
 };
 
 export default nextConfig;
